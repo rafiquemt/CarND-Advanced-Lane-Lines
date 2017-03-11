c01f107e9233e7b63f9bc71d8c5a8b211d114755
Tariq Rafique <mrafique@salesforce.com>
2017-03-11 12:15:16 -0800

    wip

--- p4.py
+++ p4.py
@@ -2,6 +2,7 @@
    Project 4 - Advanced Lane Finding
    Tariq Rafique
 """
+# pylint: disable=I0011,E1101,C0111,C0103,C0301,W0611
 import glob
 import os.path
 import pickle
@@ -9,24 +10,12 @@ import pickle
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
+from moviepy.editor import VideoFileClip
 
 import cv2
 
 
-"""
-    The goals / steps of this project are the following:
-    Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
-    Apply a distortion correction to raw images.
-    Use color transforms, gradients, etc., to create a thresholded binary image.
-    Apply a perspective transform to rectify binary image ("birds-eye view").
-    Detect lane pixels and fit to find the lane boundary.
-    Determine the curvature of the lane and vehicle position with respect to center.
-    Warp the detected lane boundaries back onto the original image.
-    Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
-"""
-
-
-def calibrate_camera(folderPath='camera_cal/calibration*.jpg'):
+def calibrate_camera(folder_path='camera_cal/calibration*.jpg'):
     """
     Return the calibration matrix to use for the calibrate_camera
     """
@@ -36,7 +25,7 @@ def calibrate_camera(folderPath='camera_cal/calibration*.jpg'):
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d points in real world space
     imgpoints = []  # 2d points in image plane.
-    images = glob.glob(folderPath)
+    images = glob.glob(folder_path)
 
     for fname in images:
         img = cv2.imread(fname)
@@ -46,10 +35,10 @@ def calibrate_camera(folderPath='camera_cal/calibration*.jpg'):
         ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
 
         # If found, add object points, image points
-        if ret == True:
+        if ret is True:
             objpoints.append(objp)
             imgpoints.append(corners)
-        elif ret == False:
+        elif ret is False:
             print("****** FAILED to find chessboard in", fname)
 
     return cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
@@ -61,11 +50,23 @@ def test_calibration(image, mtx, dist, folderPath='camera_cal'):
     cv2.imwrite('output_images/calibrated_' + image, undist)
 
 
-def process_frame():
-    pass
+def process_frame(img, mtx, dist):
+    undistorted_image = cv2.undistort(img, mtx, dist, None, mtx)
 
-
+    final = undistorted_image
+    return final
 def main():
+    """
+        The goals / steps of this project are the following:
+        Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
+        Apply a distortion correction to raw images.
+        Use color transforms, gradients, etc., to create a thresholded binary image.
+        Apply a perspective transform to rectify binary image ("birds-eye view").
+        Detect lane pixels and fit to find the lane boundary.
+        Determine the curvature of the lane and vehicle position with respect to center.
+        Warp the detected lane boundaries back onto the original image.
+        Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
+    """
     pickle_file = 'cal_data/cal_data.p'
     print("Hello")
     if os.path.exists(pickle_file):
@@ -77,8 +78,14 @@ def main():
         x, mtx, dist, x, x = calibrate_camera()
         pickle.dump({'mtx': mtx, 'dist': dist}, open(pickle_file, 'wb'))
     test_calibration('calibration1.jpg', mtx, dist)
-    test_calibration('calibration4.jpg', mtx, dist)
     test_calibration('calibration5.jpg', mtx, dist)
 
+
+    test_videos = ['project_video.mp4'] # 'challenge_video.mp4', 'harder_challenge_video.mp4'
+
+    for vid_file in test_videos:
+        clip = VideoFileClip(vid_file)
+        output_clip = clip.fl_image(lambda img: process_frame(img, mtx, dist))
+        output_clip.write_videofile('output_'+vid_file, audio=False, threads=4)
 
 main()
